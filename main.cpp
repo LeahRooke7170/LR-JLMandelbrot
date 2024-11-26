75542e204c19e0cdf9e23c737108fd8db37c6890
jbones5263 <jbones5263@comcast.net>
2024-11-26 15:00:05 -0800

    update

--- main.cpp
+++ main.cpp
@@ -1,7 +1,35 @@
 #include"ComplexPlane.h"
 
+
 int main()
 {
 	cout << "when the project compiles :D" << endl;
 	cout << ">:)" << endl;
+
+	VideoMode video;
+	int screenH = video.getDesktopMode().height;
+	int screenW = video.getDesktopMode().width;
+
+	RenderWindow window(VideoMode(screenW, screenH), "opened");
+	CircleShape cir(100.f);
+	cir.setFillColor(sf::Color::Blue);
+
+	while (window.isOpen())
+	{
+		Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == Event::Closed) { window.close(); }
+
+			if (Keyboard::isKeyPressed(Keyboard::Escape)) { window.close(); }
+		}
+
+
+		window.clear();
+		window.draw(cir);
+		window.display();
+
+
+	}
+	return 0;
 }
